67ec4052123477281096eb0650d6ee56d73a2ed3
Taylor Rice <taylorrice@Taylors-MacBook-Air.local>
2023-07-31 13:54:26 -0400

    skeletion of goodreads data pull

--- data_ingestion_dag.py
+++ data_ingestion_dag.py
@@ -1,10 +1,11 @@
  import datetime
 
- from airflow import DAG
- from airflow.operators.empty import EmptyOperator
- from airflow.providers.http.operators.http import SimpleHttpOperator
- from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
- from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
+from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+from airflow.providers.amazon.aws.operators.redshift import RedshiftSQLOperator
 
 '''
 This DAG will pull NYT Book Review data and GoodReads data and compare reviews on popular books.
@@ -14,6 +15,7 @@ It will flag any book where the review values differ beyond a given threshold.
 S3_BUCKET = "s3://veda-project-mwaa"
 DATE = datetime.date().isoformat()
 NYT_S3_KEY = f"/nyt/{DATE}/bestsellers.json"
+GOODREADS_S3_KEY = f"/goodreads/{DATE}/reviews.json"
 
  with DAG(
      dag_id="data_ingestion",
@@ -39,17 +41,48 @@ NYT_S3_KEY = f"/nyt/{DATE}/bestsellers.json"
     )
 
     nyt_data_s3_to_redshift_task = S3ToRedshiftOperator(
-        task_id="transfer_s3_to_redshift",
+        task_id="transfer_nyt_s3_to_redshift",
         redshift_conn_id=conn_id_name,
         s3_bucket=S3_BUCKET,
         s3_key=NYT_S3_KEY,
         schema="PUBLIC",
         table=REDSHIFT_TABLE,
-        copy_options=["csv"],
+        copy_options=["json"],
     )
 
+    get_booklist_task = RedshiftSQLOperator(
+        task_id='get_booklist',
+        sql= ### WRITE SQL
+    )
+
+    pull_goodreads_data_task = SimpleHttpOperator(
+        task_id="pull_goodreads_data",
+        method="GET",
+        endpoint="http://api.nytimes.com/svc/books/v3/lists/current/full-overview.json?api-key=An6NraSaBG3oUkAHretwK4m3wZUSK3wZ",
+        response_filter=lambda response: response.json()["results"]["lists"]["list_id","books"]["author","rank","title","description","weeks_on_list"],
+        dag=dag,
+    )
+
+    goodreads_data_to_S3_task = S3CreateObjectOperator(
+        task_id="create_goodreads_data_s3_object",
+        s3_bucket=S3_BUCKET,
+        s3_key=GOODREADS_S3_KEY,
+        data=ti.xcom_pull(task_ids='pull_goodreads_data'),
+        replace=True,
+    )
+
+    goodreads_data_s3_to_redshift_task = S3ToRedshiftOperator(
+        task_id="transfer_goodreads_s3_to_redshift",
+        redshift_conn_id=conn_id_name,
+        s3_bucket=S3_BUCKET,
+        s3_key=GOODREADS_S3_KEY,
+        schema="PUBLIC",
+        table=REDSHIFT_TABLE,
+        copy_options=["json"],
+    )
 
 
      end = EmptyOperator(task_id="end")
 
-     start >> pull_nytimes_data >> nyt_data_to_S3_task >> end
+     start >> pull_nytimes_data >> nyt_data_to_S3_task >> nyt_data_s3_to_redshift_task >> get_booklist_task
+     get_booklist_task >> pull_goodreads_data_task >> goodreads_data_to_S3_task >> goodreads_data_s3_to_redshift_task
