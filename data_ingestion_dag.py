9d047a3266b55b364e1e2ffc8d9f6f7226d81d43
Taylor Rice <taylorrice@Taylors-MacBook-Air.local>
2023-07-30 21:30:42 -0400

    pull nyt bestsellers and upload to S3

--- data_ingestion_dag.py
+++ data_ingestion_dag.py
@@ -2,12 +2,16 @@
 
  from airflow import DAG
  from airflow.operators.empty import EmptyOperator
+ from airflow.providers.http.operators.http import SimpleHttpOperator
+ from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
 
 '''
 This DAG will pull NYT Book Review data and GoodReads data and compare reviews on popular books.
 It will flag any book where the review values differ beyond a given threshold.
 
 '''
+S3_BUCKET = "s3://veda-project-mwaa"
+DATE = datetime.date().isoformat()
 
  with DAG(
      dag_id="data_ingestion",
@@ -16,6 +20,24 @@ It will flag any book where the review values differ beyond a given threshold.
  ):
      start = EmptyOperator(task_id="start")
 
+     pull_nytimes_data_task = SimpleHttpOperator(
+        task_id="pull_nytimes_data",
+        method="GET",
+        endpoint="http://api.nytimes.com/svc/books/v3/lists/current/full-overview.json?api-key=An6NraSaBG3oUkAHretwK4m3wZUSK3wZ",
+        response_filter=lambda response: response.json()["results"]["lists"]["list_id","books"]["author","rank","title","description","weeks_on_list"],
+        dag=dag,
+    )
+
+    nyt_data_to_S3_task = S3CreateObjectOperator(
+        task_id="create_nyt_data_s3_object",
+        s3_bucket=S3_BUCKET,
+        s3_key=f"/nyt/{date}/bestsellers.json",
+        data=ti.xcom_pull(task_ids='pull_nytimes_data'),
+        replace=True,
+    )
+
+
+
      end = EmptyOperator(task_id="end")
 
-start >> end
+     start >> pull_nytimes_data >> nyt_data_to_S3_task >> end
